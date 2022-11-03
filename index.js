2e615e2d3b05fc610675723e45003217fae4e78b
AsukFavour <favourasuk@gmail.com>
2022-11-03 11:00:09 +0100

    functionality for side menu bar

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const lose = document.getElementById('close');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+};
+
+if (lose){
+    lose.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+};
