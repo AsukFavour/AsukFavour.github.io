5eb3777652d02f3a5741f633af0f67fd7e5473c9
AsukFavour <favourasuk@gmail.com>
2022-11-08 08:22:02 +0100

    created about page made pages responsive

--- index.js
+++ index.js
@@ -15,3 +15,28 @@ if (lose){
         nav.classList.remove('active');
     })
 };
+
+
+// products
+
+const bigImg = document.getElementById("main-img");
+const smallImg = document.querySelectorAll('.small-img');
+
+
+
+
+smallImg.forEach(
+
+    data =>{
+        data.addEventListener('click', function(e){
+            e.preventDefault;
+            bigImg.src = data.src;
+        })
+        
+    }
+)
+
+// smallImg[0].onclick =() => bigImg.src = smallImg[0].src;
+// smallImg[1].onclick =() => bigImg.src = smallImg[1].src;
+// smallImg[2].onclick =() => bigImg.src = smallImg[2].src;
+// smallImg[3].onclick =() => bigImg.src = smallImg[3].src;
