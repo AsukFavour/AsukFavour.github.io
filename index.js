e54c9eb946ffab12f71fb335193daecde3819ca1
AsukFavour <favourasuk@gmail.com>
2023-01-25 11:20:22 +0100

    add value in sporuct html

--- index.js
+++ index.js
@@ -1,71 +1,57 @@
-'use strict';
+"use strict";
 
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
-const lose = document.getElementById('close');
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const lose = document.getElementById("close");
 
-if (bar){
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
-};
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
 
-if (lose){
-    lose.addEventListener('click', () =>{
-        nav.classList.remove('active');
-    })
-};
-
+if (lose) {
+  lose.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
 
 // products
 
 const bigImg = document.getElementById("main-img");
-const smallImg = document.querySelectorAll('.small-img');
+const smallImg = document.querySelectorAll(".small-img");
 
-
-
-
-smallImg.forEach(
-
-    data =>{
-        data.addEventListener('click', function(e){
-            e.preventDefault;
-            bigImg.src = data.src;
-        })
-        
-    }
-)
+smallImg.forEach((data) => {
+  data.addEventListener("click", function (e) {
+    e.preventDefault;
+    bigImg.src = data.src;
+  });
+});
 
 // add tgo cart
 const addCart = [];
 const product = document.getElementById("productsId");
-const cartIcon = document.getElementsByClassName('buttons');
-const selectElement = document.querySelector('#select1');
-const amount =document.getElementById('size');
+const cartIcon = document.getElementsByClassName("buttons");
+const selectElement = document.querySelector("#select1");
+const amount = document.getElementById("size");
 const names = document.getElementById("name");
 const price = document.getElementById("price");
-const containerItem = document.querySelector('items')
+const containerItem = document.querySelector("items");
 
-
-const carty= Array.from(cartIcon)
+const carty = Array.from(cartIcon);
 console.log(amount);
 
-carty.forEach(
-    
-    (cart,i) => {
-        cart.addEventListener('click', (a)=>{
-            a.preventDefault;
-            addCart.push({name: names.innerText,
-                          amount: amount.value,
-                          selectElement: selectElement.value,
-                         price : price.innerText,
-                        bigImg : bigImg.src});
+carty.forEach((cart, i) => {
+  cart.addEventListener("click", (a) => {
+    a.preventDefault;
+    addCart.push({
+      name: names.innerText,
+      amount: amount.value,
+      selectElement: selectElement.value,
+      price: price.innerText,
+      bigImg: bigImg.src,
+    });
 
-            console.log( addCart)
-
-        })
- 
-}
-)
-
-items.innerH
+    console.log(addCart);
+  });
+});
