5f58fdddb35c59691489ddef43dbc30897497b05
AsukFavour <favourasuk@gmail.com>
2023-03-18 13:52:38 +0100

    resuable navbar

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const lose = document.getElementById("close");
 
+// navbar
+class navbar extends HTMLElement{
+  connectedCallback(){
+    this.innerHTML = `<section id="header">
+    <a href="#"><img src="images/The_North_Face-logo.png" alt=""  class="logo"></a>
+
+    <div >
+    <ul id="navbar">
+        <li> <a href="index.html" >Home</a></li>
+        <li> <a href="shop.html">Shop</a></li>
+        <li> <a href="contact.html">contact</a></li>
+        <li> <a href="about.html">about</a></li>
+        <li id="lg-cart"> <a href="#"><i class="fi fi-br-shopping-bag"></i></a></li>
+        <a href="cart.html" id="close"><i class="fi fi-br-x"></i></a>
+    </ul>
+    </div>
+
+    <div id="mobile">
+        <a href="#"><i class="fi fi-br-shopping-bag"></i></a>
+        <i id="bar" class="fi fi-br-menu-burger"></i>
+    </div>
+
+</section>`
+  }
+};
+
+customElements.define('app-navbar', navbar);
+
 if (bar) {
   bar.addEventListener("click", () => {
     nav.classList.add("active");
